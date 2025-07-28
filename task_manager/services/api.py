b65b9b21e5d32e77411dce503a8ab2dca4c41171
adhwaith127 <219108345+adhwaith127@users.noreply.github.com>
2025-07-28 09:37:53 +0530

    syntactical modification

--- task_manager/services/api.py
+++ task_manager/services/api.py
@@ -1,25 +1,22 @@
 import frappe
-from frappe.utils import format_datetime, time_diff_in_hours,today
+from frappe.utils import format_datetime, time_diff_in_hours, today
 from frappe.utils.data import getdate
 from collections import defaultdict
-from datetime import datetime
-from datetime import date
+from datetime import datetime, date, timedelta
+import calendar
 
-def get_checkin_data(from_date=None,to_date=None,specific_date=None):
 
+def get_checkin_data(from_date=None, to_date=None, specific_date=None):
     if from_date and to_date:
-        date_condition="DATE(ec.time) BETWEEN %s AND %s"
-        params=[from_date,to_date]
+        date_condition = "DATE(ec.time) BETWEEN %s AND %s"
+        params = [from_date, to_date]
     elif specific_date:
-        date_condition="DATE(ec.time) = %s"
-        params=[specific_date]
-    elif not from_date and not to_date and not specific_date:
-        date_condition="DATE(ec.time) = %s"
-        params=[today()]
+        date_condition = "DATE(ec.time) = %s"
+        params = [specific_date]
     else:
         frappe.throw("Invalid date parameters. Provide either both from_date and to_date, or specific_date, or leave all empty for today's data.")
 
-    base_query="""SELECT 
+    base_query = """SELECT 
                 ec.employee,
                 ec.time,
                 ec.log_type,
@@ -31,13 +28,18 @@ def get_checkin_data(from_date=None,to_date=None,specific_date=None):
             WHERE {date_condition}
             ORDER BY ec.employee ASC, ec.time ASC""".format(date_condition=date_condition)
     try:
-        emp_data=frappe.db.sql(base_query,params,as_dict=True)
+        emp_data = frappe.db.sql(base_query, params, as_dict=True)
         if not emp_data:
-            return {"message": "No checkin data found for the specified date range"}
-        
+            if specific_date and not from_date and not to_date:
+                return {"message": f"No checkin data found for {specific_date}"}
+            elif not specific_date and from_date and to_date:
+                return {"message": f"No checkin data found between {from_date} and {to_date}"}
+            return {"message": "No checkin data found for the specified date"}
+
         return emp_data
     except Exception as e:
-        return {"error":f"DB fetch error {str(e)}"}
+        frappe.log_error(f"DB error: {str(e)}", "Check-in Fetch Failure")
+        return {"error": "Failed to fetch check-in data."}
 
 
 def sort_checkin_data(emp_data):
@@ -47,74 +49,170 @@ def sort_checkin_data(emp_data):
         grouped_data[entry['employee']][date_str].append(entry)
     return grouped_data
 
+
 def calculate_work_hours(grouped_data):
-    result=[]
-    for emp,days in grouped_data.items():
-        for date,logs in days.items():
-            logs=sorted(logs,key=lambda p:p['time'])
-            total_hours=0.0
-            i=0
-            while i<len(logs)-1:
-                log1=logs[i]
-                log2=logs[i+1]
+    result = []
+    for emp, days in grouped_data.items():
+        for date, logs in days.items():
+            logs = sorted(logs, key=lambda p: p['time'])
+            total_hours = 0.0
+            i = 0
+            while i < len(logs) - 1:
+                log1 = logs[i]
+                log2 = logs[i + 1]
 
                 in_condition = log1['log_type'] == "IN"
                 out_condition = log2['log_type'] == "OUT"
-                
-                if in_condition and out_condition:
-                    log1_time=log1['time']
-                    log2_time=log2['time']
-                    shift_time=time_diff_in_hours(log2_time,log1_time)
-                    shift_time=round(shift_time,2)
-                    total_hours+=shift_time
-                    i+=2
-                else:
-                    shift_time=0.0
-                    total_hours+=shift_time
-                    i+=1
 
-            entry_time=(logs[0]['time']).strftime("%H:%M")
-            exit_time=(logs[-1]['time']).strftime("%H:%M")
+                if in_condition and out_condition:
+                    log1_time = log1['time']
+                    log2_time = log2['time']
+                    shift_time = time_diff_in_hours(log2_time, log1_time)
+                    shift_time = round(shift_time, 2)
+                    total_hours += shift_time
+                    i += 2
+                else:
+                    shift_time = 0.0
+                    total_hours += shift_time
+                    i += 1
+
+            entry_time = (logs[0]['time']).strftime("%H:%M")
+            exit_time = (logs[-1]['time']).strftime("%H:%M")
             result.append({
-                "employee":emp,
-                "department":logs[0]['department'],
-                "date":date,
-                "Work Time":total_hours,
-                "entry":entry_time,
-                "exit":exit_time,
+                "employee": emp,
+                "department": logs[0]['department'],
+                "date": date,
+                "work_time": total_hours,
+                "entry": entry_time,
+                "exit": exit_time,
+                "result_type": "daily_summary"
             })
 
-    return {"result":result,"total_count":len(result)}
+    return {"result": result, "total_count": len(result)}
 
 
-def checkin_for_date_range(data):
-    summary_data=defaultdict(lambda:{
-        'total_work_hours':0.0,
-        'days':0,
-        'latest_entry':None,
-        'earliest_exit':None,
+def checkin_data_for_date_range(data):
+    summary_data = defaultdict(lambda: {
+        'total_work_hours': 0.0,
+        'days': 0,
+        'latest_entry': None,
+        'earliest_exit': None,
         'department': None
     })
 
     for record in data['result']:
-        name=record['employee']
-        dept=record['department']
+        name = record['employee']
+        dept = record['department']
 
-        emp=summary_data[name]  # since this (summary_data) is a default dict 
-        emp['total_work_hours']+=record['Work Time'] # it creates an empty dict with fields declared above 
-        emp['days']+=1          # siince no such key exist in db currently
-        if emp['department']==dept:
+        emp = summary_data[name]
+        emp['total_work_hours'] += record['work_time']
+        emp['days'] += 1
+        if emp['department'] == dept:
             pass
         else:
             emp['department'] = dept
-        
-    result={}
-    for name,stats in summary_data.items():
-        result[name]={
-            "average_work_hours":round(stats['total_work_hours']/stats['days'],2),
-            "department":stats['department']
+
+    result = []
+    for name, stats in summary_data.items():
+        if stats['department'] is None:
+            department = "No department data available"
+        else:
+            department = stats['department']
+        employee_summary = {
+            "employee": name,
+            "department": department,
+            "average_work_hours": round(stats['total_work_hours'] / stats['days'], 2),
+            "total_working_days": stats['days'],
+            "result_type": "daterange_summary"
         }
-    return {"result":result,"total_count":len(result)}
+        result.append(employee_summary)
+    return {"result": result, "total_count": len(result)}
+
+
+def working_days_count(first_day, last_day):
+    total_working_days = 0
+    current_date = first_day
+    while current_date <= last_day:
+        if current_date.weekday() < 5:
+            total_working_days += 1
+        current_date += timedelta(days=1)
+    return total_working_days
+
+
+def calculate_monthly_average(employee_data, working_days, is_current_month, first_day, last_day):
+    employee_monthly_stats = defaultdict(lambda: {
+        'total_work_hours': 0.0,
+        'total_days_worked': 0,
+        'department': None
+    })
+
+    for record in employee_data:
+        emp_name = record['employee']
+        employee_monthly_stats[emp_name]['total_work_hours'] += record['work_time']
+        employee_monthly_stats[emp_name]['total_days_worked'] += 1
+        if employee_monthly_stats[emp_name]['department'] is None:
+            employee_monthly_stats[emp_name]['department'] = record['department']
+
+    result = []
+    for emp_name, m_stats in employee_monthly_stats.items():
+        if working_days > 0:
+            avg_working_hours = m_stats['total_work_hours'] / working_days
+        result.append({
+            "employee": emp_name,
+            'department': m_stats['department'],
+            'monthly_average': avg_working_hours,
+            "total_hours_worked": m_stats['total_work_hours'],
+            'total_days_worked': m_stats['total_days_worked'],
+            'total_monthly_working_days': working_days,
+            'result_type': 'monthly_average'
+        })
+
+    return {"result": result, "total_count": len(result)}
+
+
+def monthly_average(select_date):
+    if isinstance(select_date, str):
+        try:
+            select_date = datetime.strptime(select_date, "%Y-%m-%d").date()
+        except ValueError:
+            return {"error": "Invalid date format. Use YYYY-MM-DD."}
+    else:
+        select_date = select_date
+
+    year = select_date.year
+    month = select_date.month
+    today_date = date.today()
+
+    first_day = date(year, month, 1)
+
+    if year == today_date.year and month == today_date.month:
+        last_day = today_date
+        is_current_month = True
+    elif select_date < today_date:
+        last_day = date(year, month, calendar.monthrange(year, month)[1])
+        is_current_month = False
+    else:
+        return {"error": "Please enter a valid date"}
+
+    working_days = working_days_count(first_day, last_day)
+
+    checkin_data = get_checkin_data(from_date=first_day.strftime('%Y-%m-%d'), to_date=last_day.strftime('%Y-%m-%d'))
+
+    if isinstance(checkin_data, dict) and ('error' in checkin_data or 'message' in checkin_data):
+        return checkin_data
+
+    grouped_data = sort_checkin_data(checkin_data)
+    processed_data = calculate_work_hours(grouped_data)
+    monthly_stats = calculate_monthly_average(
+        processed_data['result'],
+        working_days,
+        is_current_month,
+        first_day,
+        last_day
+    )
+
+    return monthly_stats
+
 
 @frappe.whitelist(allow_guest=True)
 def fetch_checkins(from_date=None, to_date=None, specific_date=None):
@@ -123,20 +221,25 @@ def fetch_checkins(from_date=None, to_date=None, specific_date=None):
             frappe.throw("Provide To date")
         if to_date and not from_date:
             frappe.throw("Provide From date")
+        if from_date and specific_date or to_date and specific_date:
+            frappe.throw("Provide either Date range or a single date")
+
+        if specific_date is None and from_date is None and to_date is None:
+            specific_date = today()  #set default date as today
 
         emp_data = get_checkin_data(from_date, to_date, specific_date)
-        
+
         if isinstance(emp_data, dict) and ('error' in emp_data or 'message' in emp_data):
             return emp_data
-        
+
         grouped_data = sort_checkin_data(emp_data)
         result = calculate_work_hours(grouped_data)
 
         if from_date and to_date:
-            result = checkin_for_date_range(result)
+            result = checkin_data_for_date_range(result)
 
         return result
-    
+
     except Exception as e:
-        frappe.log_error(f"Error in fetching data from database: {str(e)}")
-        return {"error": f"Processing error: {str(e)}"}
+        frappe.log_error(str(e), "Unexpected error in fetch_checkins")
+        return {"error": "Unexpected error occurred."}
